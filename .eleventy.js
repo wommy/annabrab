91735745d8df7c56a5fb92824f2bc53eeee31ba6
wommy <tom@technomad.media>
2019-10-25 12:07:54 -0400

    netlifycms_admin the 11ty way
    netlifycms_admin static => 11ty dynamic
      changed from static.html => 11ty _template
    netlifycms_config.yml => 11ty's _static
    eleventy.config to make this work

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 module.exports = function(eleventyConfig) {
-	eleventyConfig.addPassthroughCopy("admin");
+	eleventyConfig.addPassthroughCopy("_static");
 	eleventyConfig.setBrowserSyncConfig({https: true});
 	return {
 		dir: {
