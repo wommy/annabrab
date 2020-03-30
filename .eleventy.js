0172db49722aeebec559c897cfde3bfc19190a5d
wommy <tom@technomad.media>
2020-03-30 15:08:29 -0400

    added tailwind

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = (config) => {
   config.addPassthroughCopy("./src/11ty/__static");
   config.setDataDeepMerge(true);
+  config.setTemplateFormats("njk,css");
   return {
   	dir: {
   		input: "src/11ty"
